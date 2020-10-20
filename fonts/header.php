6397478c67cabf4873a745dac827765ea9691dfe
geezy-david <73113145+geezy-david@users.noreply.github.com>
2020-10-20 11:07:22 +0200

    Add files via upload

---
+++ fonts/header.php
@@ -0,0 +1,58 @@
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="frameworks/loadingbar.css">
+  <link rel="stylesheet" href="frameworks/style.css">
+  <script src="jquery.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="frameworks/jquery.loadingbar.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src='frameworks/ajax.js'></script>
+
+ <style type="text/css">
+ body {
+ background-color: #ccc;
+ }
+  .kk{
+  height: 100%;
+   z-index: 1; 
+   position: fixed; 
+   top: 0; left: 0; 
+   width: 100%;
+    background-color: black;
+    opacity: 0.3;
+  }
+  #home .link1{
+    color:black;
+  }
+  #message .link2{
+    color:black;
+  }
+  a{
+    text-decoration: none;
+    color: white;
+
+  }
+  
+  a:hover{
+    color:black;
+  }
+     .b{
+            border:1px solid #007bff;
+            margin-top:30px;
+            margin-bottom:10px;
+            }
+            .form-control:focus{
+                box-shadow:none;
+            }
+            .red{
+            	border:2px solid red;
+            }
+  </style> 
+
